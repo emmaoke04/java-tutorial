fee9088b7688fe2711e73b789feb18437de61071
emmaoke04 <emmanueloke44@gmail.com>
2022-02-27 17:05:37 +0100

    do while resolved

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -4,19 +4,27 @@ public class DoWhileLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int mark = 50;
+		int mark = 40;
 		
 		while (mark == 50) {
 			
 			System.out.println("The mark is equal to 50");
 			
-		}else if mark = 80 {
+		}  if (mark < 50) {
+
+			System.out.println("The mark is less than  50");
+			
+
+		} else if (mark == 80) {
+			
 			System.out.println("The marks is not equal to 80");
+			
 		}else{
+			
 			System.out.println("The marks is not equal to 20");
 
 		}
 
 	}
-
+}
 
