bac45c020926d36babe6dc79e6dbc1c51292584d
emmaoke04 <96207409+emmaoke04@users.noreply.github.com>
2022-02-27 16:53:35 +0100

    added do while

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -3,6 +3,19 @@ public class DoWhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int mark = 50;
+		
+		while (mark == 50) {
+			
+			System.out.println("The mark is equal to 50");
+			
+		}else if mark = 80 {
+			System.out.println("The marks is not equal to 80");
+		}else{
+			System.out.println("The marks is not equal to 20");
+
+		}
 
 	}
 
